0ba2bcaee5c3c9b63f5923d08de6042472575abb
Ship-ctrl <thunder_97@mail.ru>
2025-10-15 19:47:16 +0300

    fixed a bit

--- include/nmea_ETL_parser.hpp
+++ include/nmea_ETL_parser.hpp
@@ -3,19 +3,19 @@
 #define GOOGLE_STRIP_LOG 1    // this must go before the #include!
 //#define GLOG_NO_ABBREVIATED_SEVERITIES
 #include <glog/logging.h>
-#include "Ship_state.h"
+//#include "Ship_state.hpp"
 #include <sstream>
 #include <iostream>
 #include "containers.hpp"
 //#include "GNSSData.h"
 
 
-int parseNMEA_ETL(const std::string& sentence, ETL_container* container)
+uint8_t parseNMEA_ETL(const std::string& sentence, ETL_container* container)
 
 /// @brief 
 /// @param container 
 /// @return 
-std::string get_ETL(ETL_container container)
+std::string get_ETL(const ETL_container& container)
 
 /// @brief test purpose generator
 /// @return kinda valid ETL msg
