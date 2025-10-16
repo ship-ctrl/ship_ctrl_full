d1d38c156e36b0d0778fb7b3cb02374614fe2155
Ship-ctrl <thunder_97@mail.ru>
2025-10-16 14:35:35 +0300

    documented a bit

--- include/nmea_ETL_parser.hpp
+++ include/nmea_ETL_parser.hpp
@@ -9,12 +9,16 @@
 #include "containers.hpp"
 //#include "GNSSData.h"
 
-
+/// @brief tokenizes a sentence to store in a container
+/// @param sentence msg itself except $TALKERIDMSGTYPE and *CHKSUM witch are already cuted and checked in HANDLER class
+/// @param container see containers.hpp 4 details
+/// @return 0 on OK, err position of error otherwise
 uint8_t parseNMEA_ETL(const std::string& sentence, ETL_container* container);
 
-/// @brief 
-/// @param container 
-/// @return 
+/// @brief get ETL from container 
+/// @note once ship class is formed the message get method may be rewrited to directly read from SHIP CLASS
+/// @param container container with fields translating from message in 1:1 without modification
+/// @return (std::string) all $TALKERID and *CHKSUM
 std::string get_ETL(const ETL_container& container);
 
 /// @brief test purpose generator
