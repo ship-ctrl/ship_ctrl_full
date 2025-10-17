36ad26c618300815e171b5e21fffadb5c9b58afb
Ship-ctrl <thunder_97@mail.ru>
2025-10-17 17:37:21 +0300

    reorganize tools as new message class

--- include/Message.hpp
+++ include/Message.hpp
@@ -1,7 +1,78 @@
-# include "nmea_tools.h"
+#pragma once
+
+#define GOOGLE_STRIP_LOG 1    // this must go before the #include!
+//#define GLOG_NO_ABBREVIATED_SEVERITIES
+#include <glog/logging.h>
+
+#include <iostream>
+#include <chrono>
+#include <time.h>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
 
 
-template<> unsigned char CalculateChecksum(const std::string& sentence)
+enum class ErrorCode {
+    Success = 0,
+    InvalidMessage,
+    OutOfRange,
+    SystemFailure,
+    CommunicationError
+};
+
+struct ErrorInfo {
+    ErrorCode code;
+    std::string message;
+    std::source_location location;
+    
+    ErrorInfo(ErrorCode c, std::string msg, 
+              std::source_location loc = std::source_location::current())
+        : code(c), message(std::move(msg)), location(loc) {}
+};
+
+template<typename T>
+using Result = std::expected<T, ErrorInfo>;
+
+class Message
+{
+public:
+    virtual ~Message() = default;
+    virtual Result<void> validate() const = 0;
+    virtual std::string name() const = 0;
+
+    /// @brief Function to calculate the NMEA 0183 checksum
+    /// @param sentence NMEA 0183 MSG $GPGAA,,,,,*CS
+    /// @return NMEA 0183 checksum as unsigned char or string
+    template<typename TT> TT  CalculateChecksum(const std::string& sentence);
+
+    /// @brief validate the NMEA 0183 checksum
+    /// @param sentence NMEA 0183 MSG $GPGAA,,,,,*CS
+    /// @return true on OK
+    bool CheckChecksum(const std::string& sentence);
+
+    /// @brief form MSG
+    /// @param sentence all exept talker ID and Checksumm 
+    /// @return complete MSG
+    std::string formNMEAMessage(const std::string& sentence, const std::string talk_id = "U0");
+
+    /// @brief systemtime in NMEA format
+    /// @return hhmmss.ss
+    std::string getCurrentTime();
+
+
+    /// @brief float to int5
+    /// @param value -1 to +1 float
+    /// @return -5 to +5 int
+    int convertFloatToInteger5(float value);
+
+protected:
+    Message() = default;
+};
+
+
+
+template<> unsigned char Message::CalculateChecksum(const std::string& sentence)
 {
     unsigned char checksum = 0;
 
@@ -18,7 +89,7 @@ template<> unsigned char CalculateChecksum(const std::string& sentence)
     return checksum;
 }
 
-template<> std::string CalculateChecksum(const std::string& sentence)
+template<> std::string Message::CalculateChecksum(const std::string& sentence)
 {
     int checksum = 0;
 
@@ -41,7 +112,6 @@ template<> std::string CalculateChecksum(const std::string& sentence)
         endIndex = sentence.length();
     }
     
-
     // Calculate the checksum
     for (size_t i = startIndex; i < endIndex; ++i)
     {
@@ -53,7 +123,7 @@ template<> std::string CalculateChecksum(const std::string& sentence)
     return stream.str();
 }
 
-bool CheckChecksum(const std::string& sentence)
+bool Message::CheckChecksum(const std::string& sentence)
 {
     size_t endIndex = sentence.find('*');
     unsigned char calcChecksum = CalculateChecksum<unsigned char>(sentence);
@@ -77,21 +147,19 @@ bool CheckChecksum(const std::string& sentence)
     #define TALKER_ID "U0" // UP -is mcu U# is user defined SN -- pos sys
 #endif
 
-std::string formNMEAMessage(const std::string& sentence, const std::string talk_id) 
+std::string Message::formNMEAMessage(const std::string& sentence, const std::string talk_id) 
 {
-    std::string checksum = CalculateChecksum<std::string>(sentence);
+    std::string checksum = Message::CalculateChecksum<std::string>(sentence);
 
     //return "$" + (std::string)TALKER_ID + sentence + "*" + checksum + "\r\n";
     std::stringstream stream;
     stream << "$" << talk_id << sentence << "*" << checksum;
     return stream.str();
 }
-
-
-
 #undef TALKER_ID
 
-std::string getCurrentTime() 
+
+std::string Message::getCurrentTime() 
 {
     // Get the current system time
     auto now = std::chrono::system_clock::now();
@@ -117,7 +185,7 @@ std::string getCurrentTime()
     return oss.str();
 }
 
-int convertFloatToInteger5(float value) {
+int Message::convertFloatToInteger5(float value) {
     // Map the float value to the range -5 to +5
     float scaledValue = value * 5;
 
